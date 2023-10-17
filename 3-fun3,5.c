ca0d21d0a7c112fbf8f954bb4f256c0abdd57fe3
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-10-17 03:48:12 -0700

    last push

---
+++ 3-fun3,5.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+* convert - converter function, a clone of itoa
+* @num: number
+* @base: base
+* @flags: argument flags
+* @params: paramater struc
+*
+* Return: string
+*/
+char *convert(long int num, int base, int flags, par_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	(void) params;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abedef" : "0123456789ABCDES";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = array[n & base];
+		n /= base;
+	} while (n != 0);
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+/**
+* put_u - prints unsigned integer numbers
+* @ap: argument pointer
+* @params: the parameters struct
+* Return: bytes printed
+*/
+int put_u(va_list ap, par_t *params)
+{
+	unsigned long l;
+
+	if (params->l_mod)
+		l = (unsigned long) va_arg(ap, unsigned long);
+	else if (params->h_mod)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+}
