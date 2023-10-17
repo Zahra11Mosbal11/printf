4addeae1ed458b82e7c9ef9e92c385c774e04c20
RayaXraya <rrayanmr2003@gmail.com>
2023-10-17 04:26:28 -0700

    b4 betty

---
+++ 4-fun.c
@@ -0,0 +1,133 @@
+
+/**
+ * put_p - Prints address
+ * @ap: Argument
+ * @params: struct of parameters
+ *
+ * Return: printed bytes
+ */
+
+int put_p(va_list ap, par_t *params)
+{
+	unsigned long int n = va_arg(ap, *unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+
+	else
+	{
+		str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+		*--str = 'x';
+		*--str = '0';
+	}
+	return (print_number(str, params));
+}
+
+/**
+ * isdigit - Checks if its a digit
+ * @c: The int to be checked
+ * Return: 0 when false, 1 when true
+ */
+
+int isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _strlen - Calculates length of a string
+ * @s: The string
+ * Return: The length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	do {
+		i++;
+		s++;
+	} while (*s != '\0');
+
+	return (i);
+}
+
+/**
+ * print_number - Prints numbers with options
+ * @str: The number string
+ * @params: Struct of parameters
+ * Return: Chars printed
+ */
+
+int print_number(char *str, par_t *params)
+{
+	unsigned int i = _strlen(str);
+	int neg = (!params->unsign && *str == '-');
+
+	if (!params->prec && *str == '0' && !str[1])
+		str = "";
+	if (neg)
+	{
+		str++;
+		i--;
+	}
+	if (params->prec != UNIT_MAX)
+	{
+		while (i++ < params->prec)
+			*--str = '0';
+	}
+	if (neg)
+		*--str = '-';
+	if (!params->minus_f)
+		return (p_n_r_sh(str, params));
+	else
+		return (p_n_l_sh(str, params));
+}
+
+/**
+ * p_n_r_sh - Print number right shift
+ * @str: String
+ * @params: Struct params
+ * Return: int
+ */
+
+int p_n_r_sh(char *str, params_t *params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = ' ';
+
+	if (params->zero_f && !params->minus_f)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-');
+	if (neg && i < params->width && pad_char == '0' && !params->minus_f)
+		str++;
+	else
+		neg = 0;
+	if ((params->plus_f && !neg2) || (!params->plus_f &&
+		params->space_f && !neg2))
+			i++;
+
+	if (neg && pad_char == '0')
+		n += _putchar('-');
+	if (params->plus_f && !neg2 && pad_char == '0' &&
+		!params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_f && !neg2 && params->space_f &&
+		!params->unsing && params->zero_f)
+		n += _putchar(' ');
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	if (neg && pad_char == ' ')
+		n += _putchar('-');
+	if (params->plus_f && !neg2 && pad_char == ' ' && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_f && params->space_f && !neg2 &&
+		!params->unsign && !params->zero_f)
+		n += _putchar(' ');
+	n += _puts(str);
+	return (n);
+}
---
+++ 5-fun.c
@@ -0,0 +1,134 @@
+/**
+ * p_n_l_sh - Print with options
+ * @str: The base
+ * @params: Struct of params
+ * Return: Chars printed
+ */
+
+int p_n_l_sh(char *str, par_t *params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = ' ';
+
+	if (params->zero_f && !params->minus_f)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-');
+	if (neg && i < params->width && pad_char == '0' && !params->minus_f)
+		str++;
+	else
+		neg = 0;
+	if (params->plus_f && !neg2 && !params->unsign)
+		n += _putchar('+'), i++;
+	else if (params->space_f && !neg2 && !params->unsign)
+		n += _putchar(' '), i++;
+	n += puts(str);
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	return (n);
+}
+
+/**
+ * put_b - print binary
+ * @ap: list
+ * @params: struct
+ * Return: bytes printed
+ */
+
+int put_b(va_list ap, par_t *params)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int c = 0;
+
+	if (params->hash_f && n)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+
+/**
+ * put_o - print octal
+ * @ap: list
+ * @params: par
+ * Return: some
+ */
+
+int put_X(va_list ap, par_t *params)
+{
+	unsigned long l;
+	int c = 0;
+	char *str;
+
+	if (params->l_mod)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_mod)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED, params);
+	if (params->hash_f && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+
+/**
+ * put_o - print octal
+ * @ap: list
+ * @params: par
+ * Return: some
+ */
+
+int put_x(va_list ap, par_t *params)
+{
+	unsigned long l;
+        int c = 0;
+        char *str;
+
+        if (params->l_mod)
+                l = (unsigned long)va_arg(ap, unsigned long);
+        else if (params->h_mod)
+                l = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                l = (unsigned int)va_arg(ap, unsigned int);
+
+        str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+        if (params->hash_f && l)
+        {
+                *--str = 'x';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+
+/**
+ * put_o - print octal
+ * @ap: list
+ * @params: par
+ * Return: some
+ */
+
+int put_o(va_list ap, par_t *params)
+{
+	unsigned long l;
+	int c = 0;
+	char *str;
+
+	if (params->l_mod)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_mod)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 8, CONVERT_UNSIGNED, params);
+	if (params->hash_f && l)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
